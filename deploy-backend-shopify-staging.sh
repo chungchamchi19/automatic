75415393efd8c6c60fa514600f62f6adc508fc1f
chungchamchi <dinhchungkhtnk29@gmail.com>
2021-03-18 16:54:24 +0700

    add

--- deploy-backend-shopify-staging.sh
+++ deploy-backend-shopify-staging.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Enter staging number:"
 read staging
+echo "What branch you want to deploy?"
+read branch
 while (($staging != 1 && $staging != 2 && $staging != 3))
 do 
   echo "Enter staging 1, 2 or 3: "
@@ -9,6 +11,7 @@ done
 echo "Prepare to deploy staging-$staging....."
 cd ../backend-shopify && \
 git checkout staging/es-shopify-staging-$staging && \
+git pull origin $branch && \
 npm update es-core && \
 yarn build && \
 git add . && \
